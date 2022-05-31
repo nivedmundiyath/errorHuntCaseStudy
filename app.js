d8b77d7df17405d97bc0c7ce5f4d289a4431c9e2
nivedmundiyath <102532951+nivedmundiyath@users.noreply.github.com>
2022-05-31 11:21:10 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors');
+//Part 1 Point 2
+const bodyParser= require('body-parser');
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+//Part 1 Point 3
+const homeRouter = require('./src/routes/homerouter');
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+//Part 1 Point 5
+
+app.listen(3000,()=>{
+    console.log("Server Ready on 3000");
+});
